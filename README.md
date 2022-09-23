55f636f6229e9e9adf89cd0260ed1822bb6af241
Cray Mend <craymend1@gmail.com>
2022-09-23 09:29:41 -0700

    init

---
+++ README.md
@@ -0,0 +1,122 @@
+# Dexcom API SDK
+
+This package is a PHP wrapper for the [Dexcom API v2](https://developer.dexcom.com/overview).
+
+### Requirements
+
+This project works with PHP 7.2+.
+
+You also need a redirect_uri to use the Dexcom OAuth.
+
+## Installation
+
+Install with composer:
+
+```
+composer require craymend/dexcom-php-sdk
+```
+
+## Examples
+
+Create an instance of Request. Be sure to set sandbox mode when testing.
+
+```php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Craymend\Dexcom\Request;
+
+$sandboxMode = Request::MODE_SANDBOX;
+// $mode = Request::MODE_PRODUCTION;
+$redirectUri = 'https://<YOUR DOMAIN>/path/to/receive/oauth/code';
+$clientId = 'your-client-id';
+$clientSecret = 'your-client-secret';
+
+// "mode" controls the baseUrl
+echo 'baseUrl: ' . $request->getBaseUrl();
+$request->setMode($sandboxMode);
+echo 'baseUrl: ' . $request->getBaseUrl();
+
+```
+
+Next complete the OAuth process to get your access token:
+
+```php
+<?php
+// Complete OAuth process
+$request = new Request();
+$request->setMode($sandboxMode);
+
+// get auth url for user to use OAuth
+echo 'OAuth url: ';
+echo $request->getAuthUrl($redirectUri, $clientId);
+
+// Get "code" from above auth url.
+// Dexcom will return the code to your redirectUri
+//   after the user logs in and agrees to give access.
+$code = 'code-returned-to-redirect-uri';
+
+// exchange code for refresh and access tokens
+$response = $request->exchangeCode($code, $redirectUri, $clientId, $clientSecret);
+
+if (!$response->getStatus()) {
+    $errors = $response->getErrors();
+    echo json_encode($errors);
+} else{
+    $data = $response->getData();
+    $accessToken = $data['access_token'];
+    $refreshToken = $data['refresh_token'];
+}
+
+echo "access_token: $accessToken<br>";
+echo "refresh_token: $refreshToken<br>";
+
+```
+
+You can also echange the refresh token for a new refresh token and access token:
+
+
+```php
+<?php
+// Exchange refresh token
+$request = new Request();
+$request->setMode($sandboxMode);
+
+$response = $request->exchangeRefreshToken($refreshToken, $redirectUri, $clientId, $clientSecret);
+
+if (!$response->getStatus()) {
+    $errors = $response->getErrors();
+    echo json_encode($errors);
+} else{
+    $data = $response->getData();
+    $accessToken = $data['access_token'];
+    $refreshToken = $data['refresh_token'];
+}
+
+echo "access_token: $accessToken<br>";
+echo "refresh_token: $refreshToken<br>";
+
+```
+
+You can now use the Dexcom API. For example, [retrieve device calibration](https://developer.dexcom.com/get-calibrations):
+
+```php
+<?php
+$request = new Request($accessToken);
+$request->setMode($sandboxMode);
+
+$uri = '/users/self/calibrations';
+$data = [
+    'startDate' => date('Y-m-d\TH:i:s', strtotime('-29 day')),
+    'endDate' => date('Y-m-d\TH:i:s')
+];
+$response = $request->get($uri, $data);
+$data = $response->getData();
+
+echo 'data: ' . json_encode($data);
+```
+
+## License
+
+MIT
---
+++ src/Request.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace Craymend\Dexcom;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
+/**
+ * Perform API calls
+ */
+final class Request
+{
+    const MODE_SANDBOX = 'sandbox';
+    const MODE_PRODUCTION = 'production';
+
+    const API_VERSION_SANDBOX = 'v2';
+    const API_VERSION_PRODUCTION = 'v2';
+
+    const BASE_URL_SANDBOX = 'https://sandbox-api.dexcom.com';
+    const BASE_URL_PRODUCTION = 'https://api.dexcom.com';
+    
+    /**
+     * @var string - "SANDBOX" | "PRODUCTION"
+     */
+    private $mode;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * @var string
+     */
+    private $accessToken;
+
+    /**
+     * Defualts to "PRODUCTION" mode
+     */
+    public function __construct($accessToken='', $mode = '')
+    {
+        $this->accessToken = $accessToken;
+        
+        $this->mode = $mode ? $mode : self::MODE_PRODUCTION;
+        $this->setMode($this->mode); // set baseUrl
+    }
+
+    /**
+     * @return null
+     */
+    public function setMode($mode){
+        $this->mode = $mode;
+
+        if($this->mode == self::MODE_SANDBOX){
+            $this->baseUrl = self::BASE_URL_SANDBOX . '/' . self::API_VERSION_SANDBOX;
+        }else{
+            $this->baseUrl = self::BASE_URL_PRODUCTION . '/' . self::API_VERSION_PRODUCTION;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseUrl(){
+        return $this->baseUrl;
+    }
+
+    /**
+	 * @return string
+	 */
+    public function getAuthUrl($redirectUri, $clientId){
+        return "$this->baseUrl/oauth2/login?client_id=$clientId&redirect_uri=$redirectUri&response_type=code&scope=offline_access";
+    }
+
+    /**
+	 * @return Response
+	 */
+    public function exchangeCode($code, $redirectUri, $clientId, $clientSecret){
+        $url = "$this->baseUrl/oauth2/token";
+
+        $data = [
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => $redirectUri,
+            'client_secret' => $clientSecret,
+            'client_id' => $clientId
+        ];
+
+        $response = $this->post('/oauth2/token', $data);
+
+        return $response;
+    }
+
+    /**
+	 * @return Response
+	 */
+    public function exchangeRefreshToken($token, $redirectUri, $clientId, $clientSecret){
+        $url = "$this->baseUrl/oauth2/token";
+
+        $data = [
+            'refresh_token' => $token,
+            'grant_type' => 'refresh_token',
+            'redirect_uri' => $redirectUri,
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret
+        ];
+
+        $response = $this->post('/oauth2/token', $data);
+
+        return $response;
+    }
+
+    /**
+	 * @return Response
+	 */
+    public function get($path, array $params)
+    {
+        return $this->sendRequest('GET', $path, $params);
+    }
+
+    /**
+	 * @return Response
+	 */
+    public function post($path, array $body)
+    {
+        return $this->sendRequest('POST', $path, $body);
+    }
+
+    /**
+	 * @return Response
+	 */
+    public function put($path, array $body)
+    {
+        return $this->sendRequest('PUT', $path, $body);
+    }
+
+    /**
+	 * @return Response
+	 */
+    public function delete($path)
+    {
+        return $this->sendRequest('DELETE', $path);
+    }
+
+    /**
+     * Reliable test/example of aruguments and endpoint.
+     * Jumpstart the use of the API from here.
+     * 
+	 * @return Response
+	 */
+    public function testEndpoint()
+    {
+        $uri = '/users/self/calibrations';
+
+        $data = [
+            'startDate' => date('Y-m-d\TH:i:s', strtotime('-29 day')),
+            'endDate' => date('Y-m-d\TH:i:s')
+        ];
+
+        $response = $this->get($uri, $data);
+        
+        return $response->getStatus();
+    }
+
+    /**
+     * @return Response
+     */
+    private function sendRequest($method, $path, array $data = null)
+    {
+        $uri = $this->baseUrl . $path;
+
+        $requestOptions = [];
+        $headers = [];
+
+        // As of 2022.09.22, Dexcom returns an HTTP response that
+        //   throws the error "Unrecognized content encoding type".
+        // Setting [decode_content => false] fixes this. 
+        // Solution found here: https://github.com/guzzle/guzzle/issues/2146 
+        $requestOptions[RequestOptions::DECODE_CONTENT] = false;
+
+        // set headers
+        if($this->accessToken){
+            $headers['Authorization'] = 'Bearer ' . $this->accessToken;
+        }
+        if ($method === 'POST' && null !== $data) {
+            $headers['content-type'] = 'application/x-www-form-urlencoded';
+        }
+        if(count($headers) > 0){
+            $requestOptions[RequestOptions::HEADERS] = $headers;
+        }
+
+        // set data
+        if ($method === 'POST' && null !== $data) {
+            $requestOptions[RequestOptions::FORM_PARAMS] = $data;
+        }else if($method === 'GET' && null !== $data){
+            $requestOptions[RequestOptions::QUERY] = $data;
+        }
+
+        // echo 'Guzzle request options: <br><br>';
+        // echo json_encode($requestOptions);
+        // echo '<br><br>';
+
+        // send request
+        try {
+            $client = new Client();
+
+            $response = $client->request($method, $uri, $requestOptions);
+
+            $data = (array) json_decode($response->getBody(), true);
+
+            return new Response(true, $data);
+        }catch (\Exception $e) {
+            $errors['errors'] = [$e->getMessage()];
+
+            return new Response(false, [], $errors);
+        }
+    }
+}
